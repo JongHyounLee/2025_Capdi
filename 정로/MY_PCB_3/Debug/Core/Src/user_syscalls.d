2aaa5daad0b1662bd8e42e8547d9acb93a72e2b9
lee jong hyoun <162541695+JongHyounLee@users.noreply.github.com>
2025-11-23 17:01:58 +0900

    asdf

---
+++ 정로/MY_PCB_3/Debug/Core/Src/user_syscalls.d
@@ -0,0 +1,1 @@
+Core/Src/user_syscalls.o: ../Core/Src/user_syscalls.c
---
+++ 정로/model_pcb/Debug/Core/Src/cubeai_preprocess.d
@@ -0,0 +1,3 @@
+Core/Src/cubeai_preprocess.o: ../Core/Src/cubeai_preprocess.c \
+ ../Core/Inc/cubeai_preprocess.h
+../Core/Inc/cubeai_preprocess.h:
